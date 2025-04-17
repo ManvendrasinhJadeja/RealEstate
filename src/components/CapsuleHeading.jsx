3b5ec63a9584412036e4a12f1ccdbc2132724db4
ManvendrasinhJadeja <jadejamanvendrasinh79@gmail.com>
2025-04-17 18:55:52 +0530

    ADD 2 new components CapsuleHeading and PrimaryBlackBtn

---
+++ src/components/CapsuleHeading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const CapsuleHeading = ({text}) => {
+  return (
+<>
+
+<div className="uppercase bg-[#f7f7f7] border-[2px] border-[#e8e8e8] rounded-[28px] h-[40px] px-[16px] py-[8px] mb-[16px] Inter300 text-[14px]">
+    {text}
+</div>
+
+</>
+  )
+}
